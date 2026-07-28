7c8c556da0bef9440b93b778606b62fcbf11e544
AmritRaj1275 <amritraj04329@gmail.com>
2026-07-28 22:43:21 +0530

    created post and get API

--- src/app.js
+++ src/app.js
@@ -10,5 +10,27 @@ app.use(express.json())
 const upload = multer({ storage: multer.memoryStorage()})
 
 // the "image" we are using is the key that is send in form-data format 
+app.post('/create-post',upload.single("image"), async (req,res) => {
+
+    const result = await uploadFile(req.file.buffer)
+    const post = await postModel.create({
+        image : result.url,
+        caption : req.body.caption
+    })
+
+    return res.status(201).json({
+        message : "post created successfully",
+        post
+    })
+})
+
+app.get('/posts', async (req,res) => {
+    const posts = await postModel.find()
+
+    return res.status(200).json({
+        message : "Posts fetched successfully",
+        posts
+    })
+})
 
 module.exports = app;
