4cab0749f3e90c3ff6ba77cfaa66c335f90b717b
AmritRaj1275 <amritraj04329@gmail.com>
2026-07-30 19:15:50 +0530

    add CreatePost jsx file

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import CreatePost from './pages/CreatePost'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/create-post' element={<CreatePost />}></Route>
+        <Route path='/about' element={<h1>About us</h1>}></Route>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
